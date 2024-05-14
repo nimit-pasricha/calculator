fdd8ec0029b5867aa6593ca5ed978a1a03ae4543
nimit-pasricha <pasricha.nimit@gmail.com>
2024-05-14 16:31:37 +0400

    Make digits appear when digit button is clicked

--- javascript.js
+++ javascript.js
@@ -16,25 +16,54 @@ function divide(num1, num2) {
 
 let firstNumber;
 let secondNumber;
-let operator;
+let operator = null;
 
 function operate(num1, num2, operation) {
   switch (operation) {
-    case "addition":
+    case "+":
       add(num1, num2);
       break;
-    case "subtraction":
+    case "-":
       subtract(num1, num2);
       break;
-    case "multiplication":
+    case "*":
       multiply(num1, num2);
       break;
-    case "division":
+    case "/":
       divide(num1, num2);
       break;
   }
 }
-
+// TODO: Make the read and the display happen in the same method
+// otherwise shit just wont work
 function displayResult() {
-    
+  let displayValue = null;
+  const display = document.querySelector(".display");
+  const buttons = document.querySelectorAll("button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      switch (button.textContent) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+          if (operator !== null) {
+            firstNumber = button.textContent;
+          } else {
+            displayValue = operate(firstNumber, button.textContent, operator);
+            firstNumber = button.textContent;
+          }
+          break;
+      }
+      display.textContent = firstNumber;
+    });
+  });
 }
+displayResult();
