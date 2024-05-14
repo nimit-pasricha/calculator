472f5d041eb51da7ab2a2c206438abf0275d8fcc
nimit-pasricha <pasricha.nimit@gmail.com>
2024-05-14 12:17:31 +0400

    Make method that calls specific operation based on user input

--- javascript.js
+++ javascript.js
@@ -13,3 +13,24 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(num1, num2, operation) {
+    switch (operation) {
+        case "addition":
+            add(num1, num2);
+            break;
+        case "subtraction":
+            subtract(num1, num2);
+            break;
+        case "multiplication":
+            multiply(num1, num2);
+            break;
+        case "division":
+            divide(num1, num2);
+            break;
+    }
+}
