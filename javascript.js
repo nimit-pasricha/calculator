339798f3d0923c465da178d707fc7601eed49cf7
nimit-pasricha <pasricha.nimit@gmail.com>
2024-05-18 08:48:07 +0400

    Change operands when user clicks buttons

--- javascript.js
+++ javascript.js
@@ -14,9 +14,9 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-let firstOperand;
-let secondOperand;
-let operator;
+let firstOperand = 0;
+let secondOperand = 0;
+let operator = null;
 
 function operate(num1, num2, operator) {
   switch (operator) {
@@ -29,4 +29,34 @@ function operate(num1, num2, operator) {
     case "/":
       return divide(num1, num2);
   }
+}
+
+function displayResult() {
+  let isFirstOperand = true;
+  let displayValue = 0;
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) =>
+    button.addEventListener("click", () => {
+      const buttonContent = button.textContent;
+      switch (buttonContent) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+          if (isFirstOperand) {
+            firstOperand = firstOperand * 10 + +buttonContent;
+            displayValue = firstOperand;
+          } else {
+            secondOperand = secondOperand * 10 + +buttonContent;
+            displayValue = secondOperand;
+          }
+      }
+    })
+  );
 }
